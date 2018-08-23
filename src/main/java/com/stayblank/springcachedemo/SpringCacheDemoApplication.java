a84eef12d2a9009640200cc46d2a73cc6b05ed34
root <Qqqq1111>
2018-08-23 10:31:52 +0800

    init

---
+++ src/main/java/com/stayblank/springcachedemo/SpringCacheDemoApplication.java
@@ -0,0 +1,12 @@
+package com.stayblank.springcachedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheDemoApplication.class, args);
+	}
+}
